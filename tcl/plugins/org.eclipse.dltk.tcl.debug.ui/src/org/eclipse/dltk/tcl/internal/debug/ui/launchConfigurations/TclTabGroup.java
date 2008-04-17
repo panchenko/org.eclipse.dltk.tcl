c4741003fbcc1c1cb6dd6b342eb0933cfd313c09
asobolev <asobolev>
2008-04-17 09:15:55 +0000

    *fixes

--- tcl/plugins/org.eclipse.dltk.tcl.debug.ui/src/org/eclipse/dltk/tcl/internal/debug/ui/launchConfigurations/TclTabGroup.java
+++ tcl/plugins/org.eclipse.dltk.tcl.debug.ui/src/org/eclipse/dltk/tcl/internal/debug/ui/launchConfigurations/TclTabGroup.java
@@ -9,16 +9,11 @@
  *******************************************************************************/
 package org.eclipse.dltk.tcl.internal.debug.ui.launchConfigurations;
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
-import org.eclipse.debug.ui.CommonTab;
 import org.eclipse.debug.ui.EnvironmentTab;
-import org.eclipse.debug.ui.IDebugUIConstants;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
-import org.eclipse.dltk.core.DLTKCore;
-import org.eclipse.dltk.launching.ScriptLaunchConfigurationConstants;
+import org.eclipse.dltk.debug.ui.launchConfigurations.ScriptCommonTab;
 import org.eclipse.dltk.tcl.internal.debug.ui.interpreters.TclInterpreterTab;
 
 public class TclTabGroup extends AbstractLaunchConfigurationTabGroup {
@@ -29,33 +24,7 @@ public class TclTabGroup extends AbstractLaunchConfigurationTabGroup {
 				new EnvironmentTab(),
 				// new SourceContainerLookupTab(),
 				// new CommonTab()
-				new CommonTab() {
-					public void performApply(
-							ILaunchConfigurationWorkingCopy configuration) {
-						super.performApply(configuration);
-						try {
-							boolean config = configuration
-									.getAttribute(
-											ScriptLaunchConfigurationConstants.ATTR_USE_INTERACTIVE_CONSOLE,
-											false);
-							if (config) {
-								configuration
-										.setAttribute(
-												IDebugUIConstants.ATTR_CAPTURE_IN_CONSOLE,
-												(String) null);
-							} else {
-								configuration
-										.setAttribute(
-												IDebugUIConstants.ATTR_CAPTURE_IN_CONSOLE,
-												(String) "true");
-							}
-						} catch (CoreException e) {
-							if (DLTKCore.DEBUG) {
-								e.printStackTrace();
-							}
-						}
-					}
-				} };
+				new ScriptCommonTab() };
 		setTabs(tabs);
 	}
 }
