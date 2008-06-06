4453d44ea40c39a54ff1e273ab1e1e6cd65752ef
apanchenk <apanchenk>
2008-06-06 10:59:30 +0000

    correctly handle tab policy preferences when inserting code templates

--- tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/templates/TclTemplateContext.java
+++ tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/templates/TclTemplateContext.java
@@ -10,15 +10,38 @@
 package org.eclipse.dltk.tcl.internal.ui.templates;
 
 import org.eclipse.dltk.core.ISourceModule;
+import org.eclipse.dltk.tcl.internal.ui.TclUI;
+import org.eclipse.dltk.ui.CodeFormatterConstants;
+import org.eclipse.dltk.ui.templates.IScriptTemplateIndenter;
 import org.eclipse.dltk.ui.templates.ScriptTemplateContext;
+import org.eclipse.dltk.ui.templates.TabExpandScriptTemplateIndenter;
+import org.eclipse.dltk.ui.text.util.TabStyle;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.templates.TemplateContextType;
 
 public class TclTemplateContext extends ScriptTemplateContext {
-	
+
 	public TclTemplateContext(TemplateContextType type, IDocument document,
 			int completionOffset, int completionLength,
 			ISourceModule sourceModule) {
 		super(type, document, completionOffset, completionLength, sourceModule);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.dltk.ui.templates.ScriptTemplateContext#getIndenter()
+	 */
+	protected IScriptTemplateIndenter getIndenter() {
+		final IPreferenceStore store = TclUI.getDefault().getPreferenceStore();
+		final TabStyle tabStyle = TabStyle.forName(store
+				.getString(CodeFormatterConstants.FORMATTER_TAB_CHAR),
+				TabStyle.TAB);
+		if (TabStyle.SPACES == tabStyle) {
+			return new TabExpandScriptTemplateIndenter(store
+					.getInt(CodeFormatterConstants.FORMATTER_TAB_SIZE));
+		}
+		return super.getIndenter();
 	}
 }
