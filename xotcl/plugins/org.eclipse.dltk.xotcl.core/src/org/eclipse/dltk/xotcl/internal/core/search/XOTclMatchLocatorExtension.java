8392ece1275d1e662824c8bcf1e824bd0a2c04a3
asobolev <asobolev>
2008-06-07 05:27:58 +0000

    * fix bug with ITcl body command

--- xotcl/plugins/org.eclipse.dltk.xotcl.core/src/org/eclipse/dltk/xotcl/internal/core/search/XOTclMatchLocatorExtension.java
+++ xotcl/plugins/org.eclipse.dltk.xotcl.core/src/org/eclipse/dltk/xotcl/internal/core/search/XOTclMatchLocatorExtension.java
@@ -2,10 +2,14 @@ package org.eclipse.dltk.xotcl.internal.core.search;
 
 import org.eclipse.dltk.ast.ASTNode;
 import org.eclipse.dltk.ast.declarations.FieldDeclaration;
+import org.eclipse.dltk.ast.declarations.MethodDeclaration;
 import org.eclipse.dltk.ast.expressions.CallExpression;
+import org.eclipse.dltk.core.IModelElement;
+import org.eclipse.dltk.core.ISourceModule;
 import org.eclipse.dltk.core.search.matching.PatternLocator;
 import org.eclipse.dltk.internal.core.search.matching.MatchingNodeSet;
 import org.eclipse.dltk.tcl.core.extensions.IMatchLocatorExtension;
+import org.eclipse.dltk.tcl.internal.core.search.TclMatchLocator;
 import org.eclipse.dltk.xotcl.core.ast.xotcl.XOTclMethodCallStatement;
 import org.eclipse.dltk.xotcl.core.ast.xotcl.XOTclProcCallStatement;
 
@@ -16,8 +20,7 @@ public class XOTclMatchLocatorExtension implements IMatchLocatorExtension {
 
 		if (node instanceof XOTclMethodCallStatement) {
 			XOTclMethodCallStatement st = (XOTclMethodCallStatement) node;
-			FieldDeclaration instanceVariable = st
-					.getInstanceVariable();
+			FieldDeclaration instanceVariable = st.getInstanceVariable();
 			CallExpression call = new CallExpression(instanceVariable, st
 					.getName(), null);
 			locator.match(call, nodeSet);
@@ -27,6 +30,12 @@ public class XOTclMatchLocatorExtension implements IMatchLocatorExtension {
 					.getCallName().getName(), null);
 			locator.match(call, nodeSet);
 		}
+	}
+
+	public IModelElement createMethodHandle(ISourceModule parent,
+			MethodDeclaration method, TclMatchLocator locator) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
