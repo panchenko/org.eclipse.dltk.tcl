17b71e8ae0e8f9401f98d546e08fc5687a362187
apanchenk <apanchenk>
2008-07-07 09:34:30 +0000

    open method action for the TCL

---
+++ tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/actions/TclOpenMethodAction.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2008 xored software, Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     xored software, Inc. - initial API and Implementation (Alex Panchenko)
+ *******************************************************************************/
+package org.eclipse.dltk.tcl.internal.ui.actions;
+
+import org.eclipse.dltk.tcl.internal.ui.TclUILanguageToolkit;
+import org.eclipse.dltk.ui.IDLTKUILanguageToolkit;
+import org.eclipse.dltk.ui.actions.OpenMethodAction;
+
+public class TclOpenMethodAction extends OpenMethodAction {
+
+	protected IDLTKUILanguageToolkit getUILanguageToolkit() {
+		return TclUILanguageToolkit.getInstance();
+	}
+
+}
