f6508976703e2e0f4ffba3614723ea52d783529c
apanchenk <apanchenk>
2008-07-16 03:33:28 +0000

    introduced new method SourceParserUtil.getModuleDeclaration(ISourceModule) since most of the time it is called without problem reporter

--- xotcl/plugins/org.eclipse.dltk.xotcl.core/src/org/eclipse/dltk/xotcl/internal/core/XOTclResolver.java
+++ xotcl/plugins/org.eclipse.dltk.xotcl.core/src/org/eclipse/dltk/xotcl/internal/core/XOTclResolver.java
@@ -87,8 +87,8 @@ public class XOTclResolver {
 		ISourceModule sourceModule = (ISourceModule) method
 				.getAncestor(IModelElement.SOURCE_MODULE);
 
-		ModuleDeclaration module = SourceParserUtil.getModuleDeclaration(
-				sourceModule, null);
+		ModuleDeclaration module = SourceParserUtil
+				.getModuleDeclaration(sourceModule);
 		return module;
 
 	}
