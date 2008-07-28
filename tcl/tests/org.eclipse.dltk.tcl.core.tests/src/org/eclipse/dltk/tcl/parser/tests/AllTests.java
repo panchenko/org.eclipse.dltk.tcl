5289c80aae5f812f51f9e5ce10012611a13e4c02
apanchenk <apanchenk>
2008-07-28 08:32:11 +0000

    don't throw IOException

--- tcl/tests/org.eclipse.dltk.tcl.core.tests/src/org/eclipse/dltk/tcl/parser/tests/AllTests.java
+++ tcl/tests/org.eclipse.dltk.tcl.core.tests/src/org/eclipse/dltk/tcl/parser/tests/AllTests.java
@@ -9,14 +9,12 @@
  *******************************************************************************/
 package org.eclipse.dltk.tcl.parser.tests;
 
-import java.io.IOException;
-
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
 public class AllTests {
 
-	public static Test suite() throws IOException {
+	public static Test suite() {
 		TestSuite suite = new TestSuite(
 				"Test for org.eclipse.dltk.tcl.parser.tests"); //$NON-NLS-1$
 		// $JUnit-BEGIN$
