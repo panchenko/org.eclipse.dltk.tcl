32efbea03ff48f51cfc2830f738fd94865e2f15c
apanchenk <apanchenk>
2008-08-26 08:03:11 +0000

    isValidToolkit() is not used anymore

--- tcl/plugins/org.eclipse.dltk.tcl.debug.ui/src/org/eclipse/dltk/tcl/internal/debug/ui/launchConfigurations/TclRemoteLaunchConfigurationTab.java
+++ tcl/plugins/org.eclipse.dltk.tcl.debug.ui/src/org/eclipse/dltk/tcl/internal/debug/ui/launchConfigurations/TclRemoteLaunchConfigurationTab.java
@@ -1,10 +1,8 @@
 package org.eclipse.dltk.tcl.internal.debug.ui.launchConfigurations;
 
-import org.eclipse.dltk.core.IDLTKLanguageToolkit;
 import org.eclipse.dltk.core.PreferencesLookupDelegate;
 import org.eclipse.dltk.debug.core.DLTKDebugPreferenceConstants;
 import org.eclipse.dltk.debug.ui.launchConfigurations.RemoteLaunchConfigurationTab;
-import org.eclipse.dltk.tcl.core.TclLanguageToolkit;
 import org.eclipse.dltk.tcl.core.TclNature;
 import org.eclipse.dltk.tcl.internal.debug.TclDebugPlugin;
 
@@ -40,12 +38,5 @@ public class TclRemoteLaunchConfigurationTab extends
 	 */
 	protected String getNatureID() {
 		return TclNature.NATURE_ID;
-	}
-
-	/*
-	 * @see org.eclipse.dltk.debug.ui.launchConfigurations.ScriptLaunchConfigurationTab#isValidToolkit(org.eclipse.dltk.core.IDLTKLanguageToolkit)
-	 */
-	protected boolean isValidToolkit(IDLTKLanguageToolkit toolkit) {
-		return (toolkit instanceof TclLanguageToolkit) ? true : false;
 	}
 }
