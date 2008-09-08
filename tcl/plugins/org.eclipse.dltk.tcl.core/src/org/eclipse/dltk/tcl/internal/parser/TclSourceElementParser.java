46a733a0a5079dd28bb4916dcba79658c81f494c
apanchenk <apanchenk>
2008-09-08 12:12:39 +0000

    new implementation of the parseSourceModule()

--- tcl/plugins/org.eclipse.dltk.tcl.core/src/org/eclipse/dltk/tcl/internal/parser/TclSourceElementParser.java
+++ tcl/plugins/org.eclipse.dltk.tcl.core/src/org/eclipse/dltk/tcl/internal/parser/TclSourceElementParser.java
@@ -12,13 +12,8 @@
  *******************************************************************************/
 package org.eclipse.dltk.tcl.internal.parser;
 
-import org.eclipse.dltk.ast.declarations.ModuleDeclaration;
-import org.eclipse.dltk.ast.parser.ISourceParserConstants;
 import org.eclipse.dltk.compiler.SourceElementRequestVisitor;
 import org.eclipse.dltk.core.AbstractSourceElementParser;
-import org.eclipse.dltk.core.DLTKCore;
-import org.eclipse.dltk.core.SourceParserUtil;
-import org.eclipse.dltk.core.ISourceModuleInfoCache.ISourceModuleInfo;
 import org.eclipse.dltk.tcl.core.TclNature;
 
 public class TclSourceElementParser extends AbstractSourceElementParser {
@@ -29,24 +24,6 @@ public class TclSourceElementParser extends AbstractSourceElementParser {
 	protected SourceElementRequestVisitor createVisitor() {
 		return new TclSourceElementRequestVisitor(this.getRequestor(), this
 				.getProblemReporter());
-	}
-
-	public void parseSourceModule(char[] contents, ISourceModuleInfo astCache,
-			char[] filename) {
-
-		ModuleDeclaration moduleDeclaration = SourceParserUtil
-				.getModuleDeclaration(filename, contents, getNatureId(),
-						getProblemReporter(), astCache,
-						ISourceParserConstants.DEFAULT);
-
-		final SourceElementRequestVisitor requestor = createVisitor();
-		try {
-			moduleDeclaration.traverse(requestor);
-		} catch (Exception e) {
-			if (DLTKCore.DEBUG) {
-				e.printStackTrace();
-			}
-		}
 	}
 
 	/*
