42c2021ecdd52ffcf0616dd0ba04f0f5b3745125
apanchenk <apanchenk>
2008-09-12 11:39:52 +0000

    hack removed

--- tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/text/TclRequirePackageMarkerResolution.java
+++ tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/text/TclRequirePackageMarkerResolution.java
@@ -13,7 +13,6 @@ import org.eclipse.dltk.launching.InterpreterContainerHelper;
 import org.eclipse.dltk.launching.ScriptRuntime;
 import org.eclipse.dltk.tcl.internal.ui.TclUI;
 import org.eclipse.dltk.ui.text.IAnnotationResolution;
-import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.source.Annotation;
 import org.eclipse.osgi.util.NLS;
@@ -59,15 +58,6 @@ final class TclRequirePackageMarkerResolution implements IMarkerResolution,
 	}
 
 	public void run(Annotation annotation, IDocument document) {
-		if (resolve()) {
-			// FIXME make proper solution
-			if (document.getLength() > 0) {
-				try {
-					document.replace(0, 1, document.get(0, 1));
-				} catch (BadLocationException e) {
-					// e.printStackTrace();
-				}
-			}
-		}
+		resolve();
 	}
 }
