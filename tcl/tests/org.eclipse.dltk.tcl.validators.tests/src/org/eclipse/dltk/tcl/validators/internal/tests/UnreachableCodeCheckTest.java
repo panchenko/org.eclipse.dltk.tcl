72363b6c5dadc48d2ac88731208c18894ff12ed3
asobolev <asobolev>
2008-09-15 04:07:40 +0000

    * unreachable code check improvements (DLTK-690)

--- tcl/tests/org.eclipse.dltk.tcl.validators.tests/src/org/eclipse/dltk/tcl/validators/internal/tests/UnreachableCodeCheckTest.java
+++ tcl/tests/org.eclipse.dltk.tcl.validators.tests/src/org/eclipse/dltk/tcl/validators/internal/tests/UnreachableCodeCheckTest.java
@@ -12,7 +12,6 @@
 
 package org.eclipse.dltk.tcl.validators.internal.tests;
 
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,52 +20,125 @@ import junit.framework.TestCase;
 
 import org.eclipse.dltk.tcl.ast.TclCommand;
 import org.eclipse.dltk.tcl.core.TclParseUtil.CodeModel;
-import org.eclipse.dltk.tcl.definitions.Scope;
 import org.eclipse.dltk.tcl.internal.validators.ICheckKinds;
 import org.eclipse.dltk.tcl.internal.validators.checks.UnreachableCodeCheck;
 import org.eclipse.dltk.tcl.parser.TclError;
 import org.eclipse.dltk.tcl.parser.TclErrorCollector;
 import org.eclipse.dltk.tcl.parser.TclParser;
-import org.eclipse.dltk.tcl.parser.definitions.DefinitionLoader;
-import org.eclipse.dltk.tcl.parser.tests.TestScopeProcessor;
+import org.eclipse.dltk.tcl.parser.definitions.DefinitionManager;
+import org.eclipse.dltk.tcl.parser.definitions.NamespaceScopeProcessor;
 import org.eclipse.dltk.tcl.parser.tests.TestUtils;
 import org.eclipse.dltk.tcl.validators.ITclCheck;
 
 public class UnreachableCodeCheckTest extends TestCase {
-	TestScopeProcessor processor = new TestScopeProcessor();
+	NamespaceScopeProcessor processor = new NamespaceScopeProcessor();
 
 	public void test001() throws Exception {
 		String source = "puts 1; return; puts 2";
-		typedCheck(source, ICheckKinds.UNREACHABLE_CODE);
+		List<Integer> errorCodes = new ArrayList<Integer>();
+		errorCodes.add(ICheckKinds.UNREACHABLE_CODE);
+		typedCheck(source, errorCodes);
 	}
 
 	public void test002() throws Exception {
 		String source = "puts 1; return;";
-		typedCheck(source);
+		List<Integer> errorCodes = new ArrayList<Integer>();
+		typedCheck(source, errorCodes);
 	}
 
 	public void test003() throws Exception {
 		String source = "puts 1; return; puts 2; puts 3; puts 4";
-		typedCheck(source, ICheckKinds.UNREACHABLE_CODE);
-	}
-
-	private void typedCheck(String source) throws Exception {
-		typedCheck(source, new ArrayList<Integer>());
-	}
-
-	private void typedCheck(String source, int errorCode) throws Exception {
 		List<Integer> errorCodes = new ArrayList<Integer>();
-		errorCodes.add(errorCode);
+		errorCodes.add(ICheckKinds.UNREACHABLE_CODE);
+		typedCheck(source, errorCodes);
+	}
+
+	public void test004() throws Exception {
+		String source = "proc my {} {puts 1; return; puts 2; puts 3; puts 4}";
+		List<Integer> errorCodes = new ArrayList<Integer>();
+		errorCodes.add(ICheckKinds.UNREACHABLE_CODE);
+		typedCheck(source, errorCodes);
+	}
+
+	public void test005() throws Exception {
+		String source = "proc namespace2 {args} {alpha;return;beta;beta;beta}; proc lala {} {}";
+		List<Integer> errorCodes = new ArrayList<Integer>();
+		errorCodes.add(ICheckKinds.UNREACHABLE_CODE);
+		typedCheck(source, errorCodes);
+	}
+
+	public void test006() throws Exception {
+		String source = "proc lala {} {proc mumu {} {alpha; return beta;beta;beta; return}; return; beta}";
+		List<Integer> errorCodes = new ArrayList<Integer>();
+		errorCodes.add(ICheckKinds.UNREACHABLE_CODE);
+		errorCodes.add(ICheckKinds.UNREACHABLE_CODE);
+		typedCheck(source, errorCodes);
+	}
+
+	public void test007() throws Exception {
+		String source = "proc lala {} {proc gamma {} {alpha; return}; proc beta {} {alpha; return}}";
+		List<Integer> errorCodes = new ArrayList<Integer>();
+		typedCheck(source, errorCodes);
+	}
+
+	public void test008() throws Exception {
+		String source = "if 1 {return} {return; puts 2}";
+		List<Integer> errorCodes = new ArrayList<Integer>();
+		errorCodes.add(ICheckKinds.UNREACHABLE_CODE);
+		typedCheck(source, errorCodes);
+	}
+
+	public void test009() throws Exception {
+		String source = "if 1 {return} {return; puts 2}; return; puts 4;";
+		List<Integer> errorCodes = new ArrayList<Integer>();
+		errorCodes.add(ICheckKinds.UNREACHABLE_CODE);
+		errorCodes.add(ICheckKinds.UNREACHABLE_CODE);
+		typedCheck(source, errorCodes);
+	}
+
+	public void test010() throws Exception {
+		String source = "after 340 {return 30; puts alpha}; cmd; return 50; puts 34";
+		List<Integer> errorCodes = new ArrayList<Integer>();
+		errorCodes.add(ICheckKinds.UNREACHABLE_CODE);
+		errorCodes.add(ICheckKinds.UNREACHABLE_CODE);
+		typedCheck(source, errorCodes);
+	}
+
+	public void test011() throws Exception {
+		String source = "switch alpha {1 {return [alpha]} 2 {return 2} 3 {return 3}}";
+		List<Integer> errorCodes = new ArrayList<Integer>();
+		typedCheck(source, errorCodes);
+	}
+
+	public void test012() throws Exception {
+		String source = "switch -- $match_type {"
+				+ " \"regexp\" { return [regexp $pattern_data $result_data] }"
+				+ "\"exact\" { return [string equal $result_data $pattern_data] }"
+				+ "\"glob\" { return [string match $result_data $pattern_data] }"
+				+ "}";
+		List<Integer> errorCodes = new ArrayList<Integer>();
+		typedCheck(source, errorCodes);
+	}
+
+	public void test013() throws Exception {
+		String source = "puts [puts 4; return; puts 5]";
+		List<Integer> errorCodes = new ArrayList<Integer>();
+		errorCodes.add(ICheckKinds.UNREACHABLE_CODE);
+		typedCheck(source, errorCodes);
+	}
+
+	public void test014() throws Exception {
+		String source = "puts beta[puts 4; return]";
+		List<Integer> errorCodes = new ArrayList<Integer>();
 		typedCheck(source, errorCodes);
 	}
 
 	private void typedCheck(String source, List<Integer> errorCodes)
 			throws Exception {
-		Scope scope = DefinitionLoader
-				.loadDefinitions(new URL(
-						"platform:///plugin/org.eclipse.dltk.tcl.tcllib/definitions/builtin.xml"));
-		TestCase.assertNotNull(scope);
-		processor.add(scope);
+		processor = DefinitionManager.getInstance().createProcessor();
+		StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+		StackTraceElement element = stackTrace[2];
+		System.out.println("%%%%%%%%%%%%%%%%Test:" + element.getMethodName());
 		TclParser parser = new TclParser();
 		TclErrorCollector errors = new TclErrorCollector();
 		List<TclCommand> module = parser.parse(source, errors, processor);
@@ -80,8 +152,10 @@ public class UnreachableCodeCheckTest extends TestCase {
 		int count = 0;
 		for (int code : errorCodes) {
 			for (TclError tclError : errors.getErrors()) {
-				if (tclError.getCode() == code)
+				if (tclError.getCode() == code) {
 					count++;
+					break;
+				}
 			}
 		}
 		TestCase.assertEquals(errorCodes.size(), count);
