0732f9f058f73cb89ccf2abcb05bf2bda9b1240c
apanchenk <apanchenk>
2008-09-24 07:42:53 +0000

    use local variable since it could be modified already

--- tcl/plugins/org.eclipse.dltk.tcl.core/src/org/eclipse/dltk/tcl/internal/core/search/mixin/TclMixinBuildVisitor.java
+++ tcl/plugins/org.eclipse.dltk.tcl.core/src/org/eclipse/dltk/tcl/internal/core/search/mixin/TclMixinBuildVisitor.java
@@ -66,7 +66,7 @@ public class TclMixinBuildVisitor extends ASTVisitor {
 			if (name.endsWith("::")) {
 				name = name.substring(0, name.length() - 2);
 			}
-			if (ns.getName().startsWith("::")) {
+			if (name.startsWith("::")) {
 				// Remove all previous
 				prefix.delete(0, prefix.length());
 				name = name.substring(2);
