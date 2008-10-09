2541545ce0894974a718af637ad3e78961a7f43d
apanchenk <apanchenk>
2008-10-09 12:32:16 +0000

    isPrivate(String name) optimization

--- tcl/plugins/org.eclipse.dltk.tcl.core/src/org/eclipse/dltk/tcl/internal/core/codeassist/TclVisibilityUtils.java
+++ tcl/plugins/org.eclipse.dltk.tcl.core/src/org/eclipse/dltk/tcl/internal/core/codeassist/TclVisibilityUtils.java
@@ -2,12 +2,14 @@ package org.eclipse.dltk.tcl.internal.core.codeassist;
 
 public class TclVisibilityUtils {
 	public static boolean isPrivate(String name) {
-		if( name.indexOf("::") != -1) {
-			String []split = name.split("::");
-			name = split[split.length-1];
+		int pos = name.lastIndexOf("::");
+		if (pos >= 0) {
+			pos = pos + 2;
+		} else {
+			pos = 0;
 		}
-		if (name.length() > 0) {
-			char c = name.charAt(0);
+		if (pos < name.length()) {
+			char c = name.charAt(pos);
 			if (Character.isUpperCase(c) || c == '_') {
 				return true;
 			}
