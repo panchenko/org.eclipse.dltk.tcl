30c677ab24617d24132ad1ec6cde8384ad049cb6
apanchenk <apanchenk>
2008-10-21 07:40:47 +0000

    NON-NLS comments

--- tcl/plugins/org.eclipse.dltk.tcl.debug.ui/src/org/eclipse/dltk/tcl/internal/debug/ui/TclWatchExpressionDelegate.java
+++ tcl/plugins/org.eclipse.dltk.tcl.debug.ui/src/org/eclipse/dltk/tcl/internal/debug/ui/TclWatchExpressionDelegate.java
@@ -4,8 +4,8 @@ import org.eclipse.dltk.internal.debug.core.model.ScriptWatchExpressionDelegate;
 
 public class TclWatchExpressionDelegate extends ScriptWatchExpressionDelegate {
 	protected String prepareExpression(String expression) {
-		if (expression.startsWith("$")) {
-			return "expr { " + expression + " }";
+		if (expression.startsWith("$")) { //$NON-NLS-1$
+			return "expr { " + expression + " }"; //$NON-NLS-1$ //$NON-NLS-2$
 		}
 		return expression;
 	}
