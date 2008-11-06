8e1b26a21fdd3486869dd5a0cb3c75d1d30f374f
apanchenk <apanchenk>
2008-11-06 11:12:45 +0000

    add '-' to word characters

--- tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/text/TclWordDetector.java
+++ tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/text/TclWordDetector.java
@@ -16,17 +16,19 @@ import org.eclipse.jface.text.rules.IWordDetector;
  */
 public class TclWordDetector implements IWordDetector {
 
-	/* (non-Javadoc)
+	/*
 	 * Method declared on IWordDetector.
 	 */
 	public boolean isWordPart(char character) {
-		return Character.isJavaIdentifierPart(character) || character == ':';
+		return Character.isJavaIdentifierPart(character) || character == ':'
+				|| character == '-';
 	}
-	
-	/* (non-Javadoc)
+
+	/*
 	 * Method declared on IWordDetector.
 	 */
 	public boolean isWordStart(char character) {
-		return Character.isJavaIdentifierPart(character) || character == '.' || character == '(' || character == ')';
+		return Character.isJavaIdentifierPart(character) || character == '.'
+				|| character == '-' || character == '(' || character == ')';
 	}
 }
