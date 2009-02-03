2b6ef1d994cf9fa006a1b098bee045f324381605
apanchenk <apanchenk>
2009-02-03 17:22:54 +0000

    build participant to identify spawnpoints

---
+++ tcl/plugins/org.eclipse.dltk.tcl.debug/src/org/eclipse/dltk/tcl/internal/debug/spawnpoint/TclSpawnpointBuildParticipantFactory.java
@@ -0,0 +1,17 @@
+package org.eclipse.dltk.tcl.internal.debug.spawnpoint;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.dltk.core.IScriptProject;
+import org.eclipse.dltk.core.builder.AbstractBuildParticipantType;
+import org.eclipse.dltk.core.builder.IBuildParticipant;
+
+public class TclSpawnpointBuildParticipantFactory extends
+		AbstractBuildParticipantType {
+
+	@Override
+	public IBuildParticipant createBuildParticipant(IScriptProject project)
+			throws CoreException {
+		return new TclSpawnpointBuildParticipant();
+	}
+
+}
