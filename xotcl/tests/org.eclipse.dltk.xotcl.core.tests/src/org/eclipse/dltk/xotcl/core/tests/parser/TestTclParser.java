4d8543dafce9b3f98aca1818395ebd248d884519
asobolev <asobolev>
2009-04-28 10:59:41 +0000

    + Initial TCL source folder reference concept implementation is done. * Some class name refactorings

--- xotcl/tests/org.eclipse.dltk.xotcl.core.tests/src/org/eclipse/dltk/xotcl/core/tests/parser/TestTclParser.java
+++ xotcl/tests/org.eclipse.dltk.xotcl.core.tests/src/org/eclipse/dltk/xotcl/core/tests/parser/TestTclParser.java
@@ -1,14 +1,14 @@
 package org.eclipse.dltk.xotcl.core.tests.parser;
 
 import org.eclipse.dltk.compiler.problem.IProblemReporter;
-import org.eclipse.dltk.tcl.core.TclParseUtil.CodeModel;
 import org.eclipse.dltk.tcl.internal.parser.TclSourceParser;
+import org.eclipse.dltk.utils.TextUtils;
 
 class TestTclParser extends TclSourceParser {
 
 	public TestTclParser(String content) {
 		this.content = content;
-		this.codeModel = new CodeModel(content);
+		this.codeModel = TextUtils.createLineTracker(content);
 		setProcessorsState(false);
 	}
 
