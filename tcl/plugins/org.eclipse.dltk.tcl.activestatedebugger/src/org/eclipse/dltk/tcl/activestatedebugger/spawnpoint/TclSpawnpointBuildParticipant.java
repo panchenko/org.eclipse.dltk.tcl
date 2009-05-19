aa4948e4adfaa5fef433f6642f8fadf08636451f
asobolev <asobolev>
2009-05-19 09:05:08 +0000

    * tcl cache fixes * some improvements

--- tcl/plugins/org.eclipse.dltk.tcl.activestatedebugger/src/org/eclipse/dltk/tcl/activestatedebugger/spawnpoint/TclSpawnpointBuildParticipant.java
+++ tcl/plugins/org.eclipse.dltk.tcl.activestatedebugger/src/org/eclipse/dltk/tcl/activestatedebugger/spawnpoint/TclSpawnpointBuildParticipant.java
@@ -1,7 +1,5 @@
 package org.eclipse.dltk.tcl.activestatedebugger.spawnpoint;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -11,17 +9,16 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.dltk.core.ISourceModule;
 import org.eclipse.dltk.core.builder.IBuildContext;
 import org.eclipse.dltk.core.builder.IBuildParticipant;
-import org.eclipse.dltk.core.builder.ISourceLineTracker;
 import org.eclipse.dltk.tcl.activestatedebugger.TclActiveStateDebuggerConstants;
-import org.eclipse.dltk.tcl.ast.StringArgument;
-import org.eclipse.dltk.tcl.ast.TclArgument;
 import org.eclipse.dltk.tcl.ast.TclCommand;
 import org.eclipse.dltk.tcl.ast.TclModule;
+import org.eclipse.dltk.tcl.indexing.SpawnpointCollector;
+import org.eclipse.dltk.tcl.indexing.SpawnpointCollector.SpawnpointInfo;
 import org.eclipse.dltk.tcl.internal.validators.TclBuildContext;
 import org.eclipse.dltk.tcl.parser.TclParserUtils;
-import org.eclipse.dltk.tcl.parser.TclVisitor;
 import org.eclipse.dltk.utils.TextUtils;
 import org.eclipse.osgi.util.NLS;
 
@@ -34,93 +31,19 @@ public class TclSpawnpointBuildParticipant implements IBuildParticipant {
 				.getSelectedCommands());
 	}
 
-	private static class SpawnpointInfo {
-		Set<String> commands;
-		int charStart;
-		int charEnd;
-	}
-
-	private static class SpawnpointCollector extends TclVisitor {
-
-		private static final String PROC_COMMAND = "proc"; //$NON-NLS-1$
-
-		private final IBuildContext buildContext;
-		private final Set<String> spawnCommands;
-
-		public SpawnpointCollector(IBuildContext buildContext,
-				Set<String> spawnCommands) {
-			this.buildContext = buildContext;
-			this.spawnCommands = spawnCommands;
-		}
-
-		private ISourceLineTracker lineTracker;
-
-		private StringArgument getStringArgument(TclCommand command, int index) {
-			if (index < command.getArguments().size()) {
-				TclArgument argument = command.getArguments().get(index);
-				if (argument instanceof StringArgument) {
-					return (StringArgument) argument;
-				}
-			}
-			return null;
-		}
-
-		@Override
-		public boolean visit(TclCommand command) {
-			if (PROC_COMMAND.equals(command.getQualifiedName())
-					&& command.getArguments().size() == 3) {
-				final StringArgument procName = getStringArgument(command, 0);
-				if (procName != null
-						&& spawnCommands.contains(procName.getValue())) {
-					return false;
-				}
-			} else if (spawnCommands.contains(command.getQualifiedName())) {
-				if (lineTracker == null) {
-					lineTracker = buildContext.getLineTracker();
-				}
-				int lineNumber = lineTracker.getLineNumberOfOffset(command
-						.getStart());
-				addSpawnpoint(lineNumber, command.getQualifiedName(), command
-						.getStart(), command.getEnd());
-			}
-			return true;
-		}
-
-		private final Map<Integer, SpawnpointInfo> spawnpoints = new HashMap<Integer, SpawnpointInfo>();
-
-		private void addSpawnpoint(int lineNumber, String commandName,
-				int start, int end) {
-			Integer lineObj = Integer.valueOf(lineNumber);
-			SpawnpointInfo info = spawnpoints.get(lineObj);
-			if (info == null) {
-				info = new SpawnpointInfo();
-				info.commands = Collections.singleton(commandName);
-				info.charStart = start;
-				info.charEnd = end;
-				spawnpoints.put(lineObj, info);
-			} else {
-				if (!info.commands.contains(commandName)) {
-					final Set<String> commandNames = new HashSet<String>();
-					commandNames.addAll(info.commands);
-					commandNames.add(commandName);
-					info.commands = commandNames;
-				}
-				if (start < info.charStart) {
-					info.charStart = start;
-				}
-				if (end > info.charEnd) {
-					info.charEnd = end;
-				}
-			}
-		}
-	}
-
 	public void build(IBuildContext context) throws CoreException {
 		if (context.getBuildType() == IBuildContext.RECONCILE_BUILD) {
 			return;
 		}
 		final IFile file = context.getFile();
 		if (file == null) {
+			return;
+		}
+		ISourceModule module = context.getSourceModule();
+		if (module.getResource() == null) {
+			return;
+		}
+		if (spawnCommands.isEmpty()) {
 			return;
 		}
 		TclModule tclModule = TclBuildContext.getStatements(context);
