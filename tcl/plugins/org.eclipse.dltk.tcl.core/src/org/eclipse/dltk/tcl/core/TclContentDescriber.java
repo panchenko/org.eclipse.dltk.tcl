706e08eb1bfbcb685c509d3a2ef7c95fca81e765
apanchenk <apanchenk>
2009-07-15 07:45:42 +0000

    update tcl content describer patterns

--- tcl/plugins/org.eclipse.dltk.tcl.core/src/org/eclipse/dltk/tcl/core/TclContentDescriber.java
+++ tcl/plugins/org.eclipse.dltk.tcl.core/src/org/eclipse/dltk/tcl/core/TclContentDescriber.java
@@ -6,37 +6,15 @@ import org.eclipse.dltk.core.ScriptContentDescriber;
 
 public class TclContentDescriber extends ScriptContentDescriber {
 	protected final static Pattern[] header_patterns = {
-			// Pattern.compile("#!\\s*.*tclsh", Pattern.MULTILINE),
-			// Pattern.compile("#!\\s*/usr/bin/tclsh", Pattern.MULTILINE),
-			// Pattern.compile("#!\\s*/usr/bin/expect", Pattern.MULTILINE),
-			// Pattern.compile("#!\\s*/usr/bin/wish", Pattern.MULTILINE),
-			Pattern.compile("^#!.*(tclsh.*|wish.*|expect.*).*",
-					Pattern.MULTILINE),
+			Pattern.compile("^#!.*(tclsh|wish|expect)", Pattern.MULTILINE),
+			Pattern
+					.compile(
+							"^#!\\s*/bin/(ba|tc)?sh\\s*\r?\n(.*\r?\n){0,4}exec .*(tclsh|expect|wish) .*",
+							Pattern.MULTILINE),
 			Pattern
 					.compile(
 							"# ;;; Local Variables?: \\*\\*\\*\\s*\r*\n# ;;; mode: t|Tcl \\*\\*\\*\\s*\r*\n# ;;; End: \\*\\*\\*",
-							Pattern.MULTILINE),
-			Pattern
-					.compile(
-							"^#!\\s*/bin/(ba|tc)?sh\\s*\r*\n#.*\\\\s*\r*\nexec .*tclsh .*",
-							Pattern.MULTILINE),
-			Pattern
-					.compile(
-							"^#!\\s*/bin/(ba|tc)?sh\\s*\r*\n#.*\\\\s*\r*\nexec .*expect .*",
-							Pattern.MULTILINE),
-			Pattern
-					.compile(
-							"^#!\\s*/bin/(ba|tc)?sh\\s*\r*\n#.*\\\\s*\r*\nexec .*wish.* .*",
-							Pattern.MULTILINE),
-			Pattern.compile(
-					"^#!\\s*/bin/(ba|tc)?sh\\s*\r*\n\\s*exec .*wish.* .*",
-					Pattern.MULTILINE),
-			Pattern.compile(
-					"^#!\\s*/bin/(ba|tc)?sh\\s*\r*\n\\s*exec .*tclsh.* .*",
-					Pattern.MULTILINE),
-			Pattern.compile(
-					"^#!\\s*/bin/(ba|tc)?sh\\s*\r*\n\\s*exec .*expect.* .*",
-					Pattern.MULTILINE) };
+							Pattern.MULTILINE) };
 
 	protected Pattern[] getHeaderPatterns() {
 		return header_patterns;
