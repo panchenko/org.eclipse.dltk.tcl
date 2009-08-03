276a518bf83be705d5668814465538003d711352
apanchenk <apanchenk>
2009-08-03 08:27:43 +0000

    specify console image

--- tcl/plugins/org.eclipse.dltk.tcl.debug.ui/src/org/eclipse/dltk/tcl/internal/console/ui/TclConsole.java
+++ tcl/plugins/org.eclipse.dltk.tcl.debug.ui/src/org/eclipse/dltk/tcl/internal/console/ui/TclConsole.java
@@ -11,13 +11,17 @@ package org.eclipse.dltk.tcl.internal.console.ui;
 
 import org.eclipse.dltk.console.ui.ScriptConsole;
 import org.eclipse.dltk.tcl.console.TclInterpreter;
+import org.eclipse.dltk.tcl.internal.debug.ui.TclDebugUIPlugin;
 import org.eclipse.osgi.util.NLS;
 
 public class TclConsole extends ScriptConsole {
 	public static final String CONSOLE_TYPE = "tcl_console"; //$NON-NLS-1$
 
 	public TclConsole(TclInterpreter interpreter, String id) {
-		super(NLS.bind(Messages.TclConsole_Name, id), CONSOLE_TYPE);
+		super(NLS.bind(Messages.TclConsole_Name, id), CONSOLE_TYPE,
+				TclDebugUIPlugin.imageDescriptorFromPlugin(
+						TclDebugUIPlugin.PLUGIN_ID,
+						"icons/obj16/console_obj.gif")); //$NON-NLS-1$
 
 		setInterpreter(interpreter);
 		setTextHover(new TclConsoleTextHover(interpreter));
