ad8f85c93c6100f7e9f9a37fcd779ba4aee8317d
apanchenk <apanchenk>
2009-10-01 11:47:50 +0000

    specify environment only once

--- tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/wizards/TclProjectWizardFirstPage.java
+++ tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/wizards/TclProjectWizardFirstPage.java
@@ -37,12 +37,11 @@ import org.eclipse.dltk.ui.wizards.IProjectWizardExtension.IValidationRequestor;
 import org.eclipse.dltk.utils.LazyExtensionManager;
 import org.eclipse.dltk.utils.LazyExtensionManager.Descriptor;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Group;
 
 final class TclProjectWizardFirstPage extends ProjectWizardFirstPage implements
 		IValidationRequestor {
@@ -57,12 +56,18 @@ final class TclProjectWizardFirstPage extends ProjectWizardFirstPage implements
 		return TclNature.NATURE_ID;
 	}
 
+	@Override
 	protected boolean interpeterRequired() {
 		/* Specially allow to create TCL project without interpreter */
 		return false;
 	}
 
 	static class ProjectTemplateDescriptor extends Descriptor<IProjectTemplate> {
+
+		private static final String ATTR_NATURE = "nature"; //$NON-NLS-1$
+		private static final String ATTR_NAME = "name"; //$NON-NLS-1$
+		private static final String ATTR_BROWSE_BUTTON = "browseButton"; //$NON-NLS-1$
+
 		final String nature;
 		final String name;
 		final String browseButton;
@@ -73,10 +78,10 @@ final class TclProjectWizardFirstPage extends ProjectWizardFirstPage implements
 		public ProjectTemplateDescriptor(TclProjectTemplateManager manager,
 				IConfigurationElement configurationElement) {
 			super(manager, configurationElement);
-			this.nature = configurationElement.getAttribute("nature");
-			this.name = configurationElement.getAttribute("name");
+			this.nature = configurationElement.getAttribute(ATTR_NATURE);
+			this.name = configurationElement.getAttribute(ATTR_NAME);
 			this.browseButton = configurationElement
-					.getAttribute("browseButton");
+					.getAttribute(ATTR_BROWSE_BUTTON);
 		}
 
 	}
@@ -109,13 +114,15 @@ final class TclProjectWizardFirstPage extends ProjectWizardFirstPage implements
 	static class ProjectWizardExtensionDescriptor extends
 			Descriptor<IProjectWizardExtension> {
 
+		private static final String ATTR_NATURE = "nature"; //$NON-NLS-1$
+
 		final String nature;
 
 		public ProjectWizardExtensionDescriptor(
 				TclProjectWizardExtensionManager manager,
 				IConfigurationElement configurationElement) {
 			super(manager, configurationElement);
-			this.nature = configurationElement.getAttribute("nature");
+			this.nature = configurationElement.getAttribute(ATTR_NATURE);
 		}
 
 	}
@@ -184,33 +191,28 @@ final class TclProjectWizardFirstPage extends ProjectWizardFirstPage implements
 			/**
 			 * @param group
 			 */
-			public void createControls(Group group) {
+			public void createControls(Composite group) {
 				fLinkRadio.doFillIntoGrid(group, 2);
 				fBrowseButton = new Button(group, SWT.PUSH);
 				fBrowseButton.setFont(group.getFont());
 				fBrowseButton.setText(this.descriptor.browseButton);
 				fBrowseButton.setEnabled(fLinkRadio.isSelected());
-				fBrowseButton.addSelectionListener(new SelectionListener() {
-					public void widgetDefaultSelected(SelectionEvent e) {
-						doLink();
-					}
-
+				fBrowseButton.addSelectionListener(new SelectionAdapter() {
 					public void widgetSelected(SelectionEvent e) {
-						doLink();
+						execute();
 					}
 				});
 				fBrowseButton.setLayoutData(StringButtonDialogField
 						.gridDataForButton(fBrowseButton, 1));
 			}
 
-			protected void doLink() {
+			protected void execute() {
 				final IProjectTemplate projectTemplate = descriptor.get();
 				if (projectTemplate != null) {
 					IProject project = acquireProject();
 					try {
 
-						projectTemplate
-								.setCurrentEnvironment(getInterpreterEnvironment());
+						projectTemplate.setCurrentEnvironment(getEnvironment());
 						IProjectTemplateOperation operation = projectTemplate
 								.configure(project, templateOperation,
 										getShell());
@@ -249,6 +251,10 @@ final class TclProjectWizardFirstPage extends ProjectWizardFirstPage implements
 				return Status.OK_STATUS;
 			}
 
+			protected boolean isSelected() {
+				return fLinkRadio.isSelected();
+			}
+
 		}
 
 		private final List<TclProjectTemplateEntry> fOptions = new ArrayList<TclProjectTemplateEntry>();
@@ -267,8 +273,8 @@ final class TclProjectWizardFirstPage extends ProjectWizardFirstPage implements
 		}
 
 		@Override
-		protected void createControls(Group group, int numColumns) {
-			super.createControls(group, numColumns);
+		protected void createModeControls(Composite group, int numColumns) {
+			super.createModeControls(group, numColumns);
 			for (TclProjectTemplateEntry entry : fOptions) {
 				entry.createControls(group);
 			}
@@ -295,7 +301,7 @@ final class TclProjectWizardFirstPage extends ProjectWizardFirstPage implements
 			for (TclProjectTemplateEntry entry : fOptions) {
 				if (field == entry.fLinkRadio) {
 					entry.fBrowseButton.setEnabled(isValidProject()
-							&& entry.fLinkRadio.isSelected());
+							&& entry.isSelected());
 					break;
 				}
 			}
@@ -306,39 +312,35 @@ final class TclProjectWizardFirstPage extends ProjectWizardFirstPage implements
 			if (super.isInWorkspace()) {
 				return true;
 			}
-			for (TclProjectTemplateEntry entry : fOptions) {
-				if (entry.fLinkRadio.isSelected()) {
-					return true;
-				}
-			}
-			return false;
+			return getSelectedEntry() != null;
+		}
+
+		@Override
+		public boolean canChangeEnvironment() {
+			return getSelectedEntry() != null;
 		}
 
 		@Override
 		public IStatus validate(IProject handle) {
 			IStatus status = super.validate(handle);
 			if (status.isOK()) {
-				for (TclProjectTemplateEntry entry : fOptions) {
-					if (entry.fLinkRadio.isSelected()) {
-						final IStatus entryStatus = entry.validate(handle);
-						if (!entryStatus.isOK()) {
-							return new ControlStatus(entryStatus.getSeverity(),
-									entryStatus.getMessage(), entry.fLinkRadio
-											.getSelectionButton());
-						}
+				final TclProjectTemplateEntry entry = getSelectedEntry();
+				if (entry != null) {
+					final IStatus entryStatus = entry.validate(handle);
+					if (!entryStatus.isOK()) {
+						return new ControlStatus(entryStatus.getSeverity(),
+								entryStatus.getMessage(), entry.fLinkRadio
+										.getSelectionButton());
 					}
 				}
 			}
 			return status;
 		}
 
-		/**
-		 * @return
-		 */
-		IProjectTemplateOperation getSelectedTemplateOperation() {
+		protected TclProjectTemplateEntry getSelectedEntry() {
 			for (TclProjectTemplateEntry entry : fOptions) {
-				if (entry.fLinkRadio.isSelected()) {
-					return entry.templateOperation;
+				if (entry.isSelected()) {
+					return entry;
 				}
 			}
 			return null;
@@ -347,24 +349,18 @@ final class TclProjectWizardFirstPage extends ProjectWizardFirstPage implements
 		/**
 		 * @return
 		 */
-		IProjectTemplate getSelectedTemplate() {
-			for (TclProjectTemplateEntry entry : fOptions) {
-				if (entry.fLinkRadio.isSelected()) {
-					return entry.descriptor.get();
-				}
-			}
-			return null;
+		protected IProjectTemplateOperation getSelectedTemplateOperation() {
+			final TclProjectTemplateEntry entry = getSelectedEntry();
+			return entry != null ? entry.templateOperation : null;
 		}
 
 		@Override
 		public void update(Observable o, Object arg) {
 			super.update(o, arg);
 			if (o instanceof NameGroup) {
-				for (TclProjectTemplateEntry entry : fOptions) {
-					if (entry.fLinkRadio.isSelected()) {
-						entry.fBrowseButton.setEnabled(isValidProject());
-						break;
-					}
+				final TclProjectTemplateEntry entry = getSelectedEntry();
+				if (entry != null) {
+					entry.fBrowseButton.setEnabled(isValidProject());
 				}
 			}
 		}
@@ -377,15 +373,22 @@ final class TclProjectWizardFirstPage extends ProjectWizardFirstPage implements
 		if (status != null && !status.isOK()) {
 			return status;
 		}
+		final TclLocationGroup.TclProjectTemplateEntry entry = ((TclLocationGroup) fLocationGroup)
+				.getSelectedEntry();
+		if (entry == null) {
+			return status;
+		}
+		final IProjectTemplate template = entry.descriptor.get();
+		if (template == null) {
+			return status;
+		}
 		for (IProjectWizardExtension extension : wizardExtensions) {
 			IProject project = null;
 			if (fNameGroup.getName().length() != 0) {
 				project = getProjectHandle();
 			}
-			IProjectTemplate template = ((TclLocationGroup) fLocationGroup)
-					.getSelectedTemplate();
 			IStatus extensionStatus = extension.validate(project,
-					getInterpreterEnvironment(), template);
+					getEnvironment(), template);
 			if (!extensionStatus.isOK()) {
 				return extensionStatus;
 			}
--- tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/wizards/TclProjectWizardSecondPage.java
+++ tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/wizards/TclProjectWizardSecondPage.java
@@ -6,8 +6,6 @@ package org.eclipse.dltk.tcl.internal.ui.wizards;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.dltk.ui.DLTKUIPlugin;
 import org.eclipse.dltk.ui.wizards.ProjectWizardFirstPage;
 import org.eclipse.dltk.ui.wizards.ProjectWizardSecondPage;
 
@@ -19,15 +17,6 @@ final class TclProjectWizardSecondPage extends ProjectWizardSecondPage {
 	@Override
 	protected void postConfigureProject(IProgressMonitor monitor)
 			throws CoreException, InterruptedException {
-		// final IProject project = getCurrProject();
-		// final IEnvironment environment = EnvironmentManager
-		// .getEnvironment(project);
-		// if (environment != null && !environment.isLocal()) {
-		// final Map options = new HashMap();
-		// options.put(DLTKCore.INDEXER_ENABLED, DLTKCore.DISABLED);
-		// options.put(DLTKCore.BUILDER_ENABLED, DLTKCore.DISABLED);
-		// DLTKCore.create(project).setOptions(options);
-		// }
 		final IProject project = getCurrProject();
 		((TclProjectCreationWizard) getWizard()).fFirstPage.postConfigure(
 				monitor, project);
@@ -36,11 +25,6 @@ final class TclProjectWizardSecondPage extends ProjectWizardSecondPage {
 
 	protected IProject acquireProject() {
 		changeToNewProject();
-		try {
-			configureEnvironment(new NullProgressMonitor());
-		} catch (CoreException e) {
-			DLTKUIPlugin.log(e);
-		}
 		return getCurrProject();
 	}
 
