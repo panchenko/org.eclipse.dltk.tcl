a0dbc416c8f7ab55d6dc037d057fc92b636e22f7
apanchenk <apanchenk>
2009-11-04 18:00:09 +0000

    decorate not safe env vars

--- tcl/plugins/org.eclipse.dltk.tcl.debug.ui/src/org/eclipse/dltk/tcl/internal/debug/ui/interpreters/TclInterpreterEnvironmentVariablesBlock.java
+++ tcl/plugins/org.eclipse.dltk.tcl.debug.ui/src/org/eclipse/dltk/tcl/internal/debug/ui/interpreters/TclInterpreterEnvironmentVariablesBlock.java
@@ -9,8 +9,21 @@
  *******************************************************************************/
 package org.eclipse.dltk.tcl.internal.debug.ui.interpreters;
 
+import org.eclipse.dltk.core.environment.IEnvironment;
+import org.eclipse.dltk.core.environment.IExecutionEnvironment;
 import org.eclipse.dltk.internal.debug.ui.interpreters.AbstractInterpreterEnvironmentVariablesBlock;
 import org.eclipse.dltk.internal.debug.ui.interpreters.AddScriptInterpreterDialog;
+import org.eclipse.dltk.launching.EnvironmentVariable;
+import org.eclipse.dltk.ui.DLTKPluginImages;
+import org.eclipse.dltk.ui.viewsupport.ImageDescriptorRegistry;
+import org.eclipse.jface.viewers.BaseLabelProvider;
+import org.eclipse.jface.viewers.DecoratingLabelProvider;
+import org.eclipse.jface.viewers.DecorationOverlayIcon;
+import org.eclipse.jface.viewers.IBaseLabelProvider;
+import org.eclipse.jface.viewers.IDecoration;
+import org.eclipse.jface.viewers.ILabelDecorator;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.swt.graphics.Image;
 
 /**
  * Control used to edit the libraries associated with a Interpreter install
@@ -20,5 +33,55 @@ public class TclInterpreterEnvironmentVariablesBlock extends
 
 	public TclInterpreterEnvironmentVariablesBlock(AddScriptInterpreterDialog d) {
 		super(d);
+	}
+
+	@Override
+	protected IBaseLabelProvider getLabelProvider() {
+		return new DecoratingLabelProvider((ILabelProvider) super
+				.getLabelProvider(), new EnvironmentVariableDecorator());
+	}
+
+	private class EnvironmentVariableDecorator extends BaseLabelProvider
+			implements ILabelDecorator {
+
+		public Image decorateImage(Image image, Object element) {
+			if (element instanceof EnvironmentVariable) {
+				final EnvironmentVariable env = (EnvironmentVariable) element;
+				if (!isSafe(env) || !env.validate().isOK()) {
+					return registry.get(new DecorationOverlayIcon(image,
+							DLTKPluginImages.DESC_OVR_WARNING,
+							IDecoration.BOTTOM_LEFT));
+				}
+			}
+			return image;
+		}
+
+		public String decorateText(String text, Object element) {
+			return text;
+		}
+
+		@Override
+		public void dispose() {
+			super.dispose();
+			registry.dispose();
+		}
+
+		private final ImageDescriptorRegistry registry = new ImageDescriptorRegistry(
+				false);
+
+		private boolean isSafe(EnvironmentVariable env) {
+			final IEnvironment environment = getInterpreterInstall()
+					.getEnvironment();
+			if (environment != null) {
+				final IExecutionEnvironment execEnvironment = (IExecutionEnvironment) environment
+						.getAdapter(IExecutionEnvironment.class);
+				if (execEnvironment != null) {
+					return execEnvironment.isSafeEnvironmentVariable(env
+							.getName());
+				}
+			}
+			return true;
+		}
+
 	}
 }
