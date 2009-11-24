e2a8913faaee33b64cd8fb666bb991a37f851489
apanchenk <apanchenk>
2009-11-24 15:56:35 +0000

    xotcl support

---
+++ xotcl/plugins/org.eclipse.dltk.xotcl.core/src/org/eclipse/dltk/xotcl/internal/core/parser/structure/XOTclClassCreate.java
@@ -0,0 +1,151 @@
+/*******************************************************************************
+ * Copyright (c) 2009 xored software, Inc.  
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html  
+ *
+ * Contributors:
+ *     xored software, Inc. - initial API and Implementation (Alex Panchenko)
+ *******************************************************************************/
+package org.eclipse.dltk.xotcl.internal.core.parser.structure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.dltk.ast.Modifiers;
+import org.eclipse.dltk.compiler.IElementRequestor.FieldInfo;
+import org.eclipse.dltk.compiler.IElementRequestor.TypeInfo;
+import org.eclipse.dltk.compiler.problem.ProblemSeverities;
+import org.eclipse.dltk.tcl.ast.StringArgument;
+import org.eclipse.dltk.tcl.ast.TclArgument;
+import org.eclipse.dltk.tcl.ast.TclCommand;
+import org.eclipse.dltk.tcl.parser.TclParser;
+import org.eclipse.dltk.tcl.parser.printer.SimpleCodePrinter;
+import org.eclipse.dltk.tcl.structure.AbstractTclCommandModelBuilder;
+import org.eclipse.dltk.tcl.structure.ITclModelBuildContext;
+import org.eclipse.dltk.tcl.structure.ITclTypeHanlder;
+import org.eclipse.dltk.tcl.structure.TclProcessorUtil;
+import org.eclipse.dltk.tcl.structure.TclTypeResolver;
+import org.eclipse.dltk.xotcl.core.IXOTclModifiers;
+
+public class XOTclClassCreate extends AbstractTclCommandModelBuilder {
+
+	public boolean process(TclCommand command, ITclModelBuildContext context) {
+		int index = 0;
+		if (index >= command.getArguments().size()) {
+			report(context, command, "Incorrect XOTcl class declaration",
+					ProblemSeverities.Error);
+			return false;
+		}
+		TclArgument name = command.getArguments().get(index++);
+		// Skip create command (optional)
+		if (isSymbol(name) && "create".equals(asSymbol(name))) {
+			if (index >= command.getArguments().size()) {
+				report(context, command, "Incorrect XOTcl class declaration",
+						ProblemSeverities.Error);
+				return false;
+			}
+			name = command.getArguments().get(index++);
+		}
+		if (!isSymbol(name)) {
+			report(context, command, "A name or 'create' command expected.",
+					ProblemSeverities.Error);
+			return false;
+		}
+		List<String> superclasses = new ArrayList<String>();
+		List<TclArgument> fields = new ArrayList<TclArgument>();
+		while (index < command.getArguments().size()) {
+			TclArgument argument = command.getArguments().get(index++);
+			if (isSymbol(argument)) {
+				String optionName = asSymbol(argument);
+				if ("-superclass".equals(optionName)) {
+					if (index < command.getArguments().size()) {
+						TclArgument sc = command.getArguments().get(index++);
+						superclasses.add(asSymbol(sc));
+					}
+				} else if ("-parameter".equals(optionName)) {
+					if (index < command.getArguments().size()) {
+						TclArgument parameters = command.getArguments().get(
+								index++);
+						for (TclArgument a : parse(parameters)) {
+							if (a instanceof StringArgument) {
+								String aa = ((StringArgument) a).getValue();
+								if (aa.startsWith("{") && aa.endsWith("}")
+										|| aa.startsWith("\"")
+										&& aa.endsWith("\'")) {
+									List<TclArgument> parts = parse(a);
+									if (!parts.isEmpty()) {
+										TclArgument first = parts.get(0);
+										if (first instanceof StringArgument) {
+											fields.add(first);
+										}
+									}
+								} else {
+									fields.add(a);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		final TypeInfo ti = new TypeInfo();
+		ti.declarationStart = command.getStart();
+		ti.nameSourceStart = name.getStart();
+		ti.nameSourceEnd = name.getEnd() - 1;
+		ti.name = asSymbol(name);
+		ti.superclasses = superclasses.toArray(new String[superclasses.size()]);
+		ti.modifiers = IXOTclModifiers.AccXOTcl;
+		ITclTypeHanlder typeHanlder = context.get(TclTypeResolver.class)
+				.resolveType(ti, command.getEnd(), ti.name + "::dummy");
+		for (TclArgument parameter : fields) {
+			final FieldInfo fi = new FieldInfo();
+			fi.name = TclProcessorUtil.asString(parameter);
+			fi.nameSourceStart = parameter.getStart();
+			fi.nameSourceEnd = parameter.getEnd() - 1;
+			fi.declarationStart = fi.nameSourceStart;
+			fi.modifiers = Modifiers.AccPrivate;
+			context.getRequestor().enterFieldCheckDuplicates(fi);
+			context.getRequestor().exitField(fi.nameSourceEnd);
+		}
+		typeHanlder.leave(context.getRequestor());
+		return false;
+	}
+
+	private List<TclArgument> parse(TclArgument argument) {
+		int offset = argument.getStart();
+		final String content;
+		if (argument instanceof StringArgument) {
+			final String value = ((StringArgument) argument).getValue();
+			int len = value.length();
+			if (len >= 2) {
+				if (value.charAt(0) == '{' && value.charAt(len - 1) == '}') {
+					content = value.substring(1, len - 1);
+					++offset;
+				} else if (value.charAt(0) == '"'
+						&& value.charAt(len - 1) == '"') {
+					content = value.substring(1, len - 1);
+					++offset;
+				} else {
+					content = value;
+				}
+			} else {
+				content = value;
+			}
+		} else {
+			content = SimpleCodePrinter.getArgumentString(argument, argument
+					.getStart());
+		}
+		TclParser parser = new TclParser();
+		parser.setGlobalOffset(offset);
+		List<TclCommand> commands = parser.parse(content);
+		List<TclArgument> result = new ArrayList<TclArgument>();
+		for (TclCommand c : commands) {
+			result.add(c.getName());
+			result.addAll(c.getArguments());
+		}
+		return result;
+	}
+}
--- xotcl/plugins/org.eclipse.dltk.xotcl.core/src/org/eclipse/dltk/xotcl/internal/core/parser/structure/XOTclModelDetector.java
+++ xotcl/plugins/org.eclipse.dltk.xotcl.core/src/org/eclipse/dltk/xotcl/internal/core/parser/structure/XOTclModelDetector.java
@@ -11,11 +11,20 @@
  *******************************************************************************/
 package org.eclipse.dltk.xotcl.internal.core.parser.structure;
 
+import org.eclipse.dltk.tcl.ast.TclArgument;
 import org.eclipse.dltk.tcl.ast.TclCommand;
+import org.eclipse.dltk.tcl.core.TclParseUtil;
+import org.eclipse.dltk.tcl.core.ITclCommandDetector.CommandInfo;
 import org.eclipse.dltk.tcl.structure.ITclModelBuildContext;
 import org.eclipse.dltk.tcl.structure.ITclModelBuilderDetector;
+import org.eclipse.dltk.tcl.structure.TclModelBuilderUtil;
+import org.eclipse.dltk.xotcl.internal.core.XOTclKeywords;
 
-public class XOTclModelDetector implements ITclModelBuilderDetector {
+public class XOTclModelDetector extends TclModelBuilderUtil implements
+		ITclModelBuilderDetector {
+
+	private static final boolean INTERPRET_CLASS_UNKNOWN_AS_CREATE = true;
+	private static final boolean INTERPRET_OBJECT_UNKNOWN_AS_CREATE = true;
 
 	public String detect(String commandName, TclCommand command,
 			ITclModelBuildContext context) {
@@ -24,11 +33,11 @@ public class XOTclModelDetector implements ITclModelBuilderDetector {
 		}
 		commandName = normalize(commandName);
 		if (CLASS.equals(commandName)) {
-			return checkClass(command, context);
+			return checkClass(command, commandName, context);
 		} else if (OBJECT.equals(commandName)) {
-			return checkObject(command, context);
+			return checkObject(command, commandName, context);
 		} else {
-			return checkInstanceOperations(command, context);
+			return checkInstanceOperations(command, commandName, context);
 		}
 	}
 
@@ -37,30 +46,151 @@ public class XOTclModelDetector implements ITclModelBuilderDetector {
 	 * @param context
 	 * @return
 	 */
-	private String checkClass(TclCommand command, ITclModelBuildContext context) {
-		// TODO Auto-generated method stub
+	private String checkClass(TclCommand command, String commandName,
+			ITclModelBuildContext context) {
+		if (command.getArguments().isEmpty()) {
+			return null;
+		}
+		TclArgument subcmd = command.getArguments().get(0);
+		if (isSymbol(subcmd)) {
+			String value = asSymbol(subcmd);
+			XOTclClassAttribute type = findXOTclType("Class");
+			if (type != null) {
+				context.addAttribute(type);
+			}
+			if (contains(value, XOTclKeywords.XOTclCommandClassArgs)) {
+				return "#Class#" + value; //$NON-NLS-1$
+			}
+			String info = checkCreateType(context, command, commandName,
+					subcmd, value);
+			if (info != null) {
+				return info;
+			}
+			if (INTERPRET_CLASS_UNKNOWN_AS_CREATE) {
+				return "#Class#create"; //$NON-NLS-1$
+			}
+		}
 		return null;
+	}
+
+	private String checkObject(TclCommand command, String commandName,
+			ITclModelBuildContext context) {
+		if (command.getArguments().isEmpty()) {
+			return null;
+		}
+		TclArgument subcmd = command.getArguments().get(0);
+		if (isSymbol(subcmd)) {
+			String value = asSymbol(subcmd);
+			XOTclClassAttribute type = findXOTclType("Object");
+			if (type != null) {
+				context.addAttribute(type);
+			}
+			if (contains(value, XOTclKeywords.XOTclCommandObjectArgs)) {
+				return "#Object#" + value;
+			}
+			String info = checkCreateType(context, command, commandName,
+					subcmd, value);
+			if (info != null) {
+				return info;
+			}
+			// // Else unknown command or create command.
+			if (INTERPRET_OBJECT_UNKNOWN_AS_CREATE) {
+				return "#Object#create";
+			}
+		}
+		return null;
+	}
+
+	private String checkCreateType(ITclModelBuildContext context,
+			TclCommand command, String commandName, TclArgument arg,
+			String value) {
+		if (value.equals("instproc") || value.equals("proc")
+				|| value.equals("set")) {
+			context.addAttribute(new XOTclClassAttribute(commandName, command
+					.getName()));
+			String info = checkCommands(value);
+			if (info != null) {
+				return info;
+			}
+		}
+		return null;
+	}
+
+	private String checkCommands(String value) {
+		if (contains(value, XOTclKeywords.XOTclCommandClassArgs)) {
+			return "#Class#" + value;
+		}
+		if (contains(value, XOTclKeywords.XOTclCommandObjectArgs)) {
+			return "#Object#" + value;
+		}
+		return null;
+	}
+
+	private boolean contains(String value, String[] commands) {
+		for (int q = 0; q < commands.length; q++) {
+			if (value.equals(commands[q])) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/**
 	 * @param command
-	 * @param context
-	 * @return
-	 */
-	private String checkObject(TclCommand command, ITclModelBuildContext context) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/**
-	 * @param command
+	 * @param commandName
 	 * @param context
 	 * @return
 	 */
 	private String checkInstanceOperations(TclCommand command,
-			ITclModelBuildContext context) {
+			String commandName, ITclModelBuildContext context) {
+		if (command.getArguments().isEmpty()) {
+			return null;
+		}
+		TclArgument subcmd = command.getArguments().get(0);
+		if (quickTestName(TclParseUtil.tclSplit(commandName))) {
+			XOTclClassAttribute type = findXOTclType(commandName);
+			if (type != null && isSymbol(subcmd)) {
+				context.addAttribute(type);
+				return check(subcmd);
+			}
+		}
+
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	/**
+	 * @param subcmd
+	 */
+	private String check(TclArgument subcmd) {
+		String value = asSymbol(subcmd);
+		if (!"create".equals(value)) {
+			String info = checkCommands(value);
+			if (info != null) {
+				return info;
+			}
+		} else {
+			return "#Class#$newInstance";
+		}
+		return "#Class#$ProcCall";
+	}
+
+	/**
+	 * @param string
+	 * @return
+	 */
+	private XOTclClassAttribute findXOTclType(String string) {
+		// TODO implement it
+		return null;
+	}
+
+	/**
+	 * @param parts
+	 * @return
+	 */
+	private boolean quickTestName(String[] parts) {
+		// TODO Auto-generated method stub
+		return true;
 	}
 
 	@SuppressWarnings("nls")
