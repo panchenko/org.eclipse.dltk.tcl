c95be9dbef34fb1c778601fe4ca4728ae22ffc08
apanchenk <apanchenk>
2009-12-09 13:37:13 +0000

    don't create indentation options if there is formatter

--- tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/preferences/TclEditorPreferencesPage.java
+++ tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/preferences/TclEditorPreferencesPage.java
@@ -9,8 +9,10 @@
  *******************************************************************************/
 package org.eclipse.dltk.tcl.internal.ui.preferences;
 
-
+import org.eclipse.dltk.compiler.util.Util;
+import org.eclipse.dltk.tcl.core.TclNature;
 import org.eclipse.dltk.tcl.internal.ui.TclUI;
+import org.eclipse.dltk.ui.formatter.ScriptFormatterManager;
 import org.eclipse.dltk.ui.preferences.AbstractConfigurationBlockPreferencePage;
 import org.eclipse.dltk.ui.preferences.EditorConfigurationBlock;
 import org.eclipse.dltk.ui.preferences.IPreferenceConfigurationBlock;
@@ -18,42 +20,37 @@ import org.eclipse.dltk.ui.preferences.OverlayPreferenceStore;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 
-
-
 public class TclEditorPreferencesPage extends
 		AbstractConfigurationBlockPreferencePage {
 
-	/*
-	 * @see org.eclipse.ui.internal.editors.text.AbstractConfigureationBlockPreferencePage#getHelpId()
-	 */
+	@Override
 	protected String getHelpId() {
-		return "";
+		return Util.EMPTY_STRING;
 	}
 
-	/*
-	 * @see org.eclipse.ui.internal.editors.text.AbstractConfigurationBlockPreferencePage#setDescription()
-	 */
+	@Override
 	protected void setDescription() {
 		String description = org.eclipse.dltk.tcl.internal.ui.preferences.TclPreferencesMessages.TCLEditorPreferencePage_general;
 		setDescription(description);
 	}
 
+	@Override
 	protected Label createDescriptionLabel(Composite parent) {
 		return null;
 	}
 
-	/*
-	 * @see org.org.eclipse.ui.internal.editors.text.AbstractConfigurationBlockPreferencePage#setPreferenceStore()
-	 */
+	@Override
 	protected void setPreferenceStore() {
 		setPreferenceStore(TclUI.getDefault().getPreferenceStore());
 	}
 
-	/*
-	 * @see org.eclipse.ui.internal.editors.text.AbstractConfigureationBlockPreferencePage#createConfigurationBlock(org.eclipse.ui.internal.editors.text.OverlayPreferenceStore)
-	 */
+	@Override
 	protected IPreferenceConfigurationBlock createConfigurationBlock(
 			OverlayPreferenceStore overlayPreferenceStore) {
-		return new EditorConfigurationBlock(this, overlayPreferenceStore);
+		int flags = EditorConfigurationBlock.FLAG_EDITOR_SMART_NAVIGATION;
+		if (!ScriptFormatterManager.hasFormatterFor(TclNature.NATURE_ID)) {
+			flags |= EditorConfigurationBlock.FLAG_TAB_POLICY;
+		}
+		return new EditorConfigurationBlock(this, overlayPreferenceStore, flags);
 	}
 }
