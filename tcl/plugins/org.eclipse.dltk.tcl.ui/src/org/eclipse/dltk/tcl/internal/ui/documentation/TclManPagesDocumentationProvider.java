cbe50fa24ee838dd38afe29dd2a79712c0955f12
apanchenk <apanchenk>
2009-12-31 11:43:51 +0000

    support interpreter specific documentation

--- tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/documentation/TclManPagesDocumentationProvider.java
+++ tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/documentation/TclManPagesDocumentationProvider.java
@@ -12,26 +12,34 @@
 package org.eclipse.dltk.tcl.internal.ui.documentation;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.Reader;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.dltk.core.IMember;
+import org.eclipse.dltk.core.IModelElement;
+import org.eclipse.dltk.core.IScriptProject;
+import org.eclipse.dltk.launching.IInterpreterInstall;
+import org.eclipse.dltk.launching.ScriptRuntime;
 import org.eclipse.dltk.tcl.internal.ui.TclUI;
-import org.eclipse.dltk.tcl.ui.manpages.ManPageLoader;
+import org.eclipse.dltk.tcl.ui.manpages.Documentation;
+import org.eclipse.dltk.tcl.ui.manpages.InterpreterDocumentation;
 import org.eclipse.dltk.tcl.ui.manpages.ManPageFolder;
+import org.eclipse.dltk.tcl.ui.manpages.ManPageLoader;
+import org.eclipse.dltk.tcl.ui.manpages.ManPageResource;
+import org.eclipse.dltk.tcl.ui.manpages.ManpagesPackage;
+import org.eclipse.dltk.ui.documentation.DocumentationUtils;
+import org.eclipse.dltk.ui.documentation.DocumentationFileResponse;
+import org.eclipse.dltk.ui.documentation.IDocumentationResponse;
 import org.eclipse.dltk.ui.documentation.IScriptDocumentationProvider;
+import org.eclipse.dltk.ui.documentation.IScriptDocumentationProviderExtension;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
 
 public class TclManPagesDocumentationProvider implements
-		IScriptDocumentationProvider {
+		IScriptDocumentationProvider, IScriptDocumentationProviderExtension {
 
-	private List folders = null;
+	private ManPageResource manPages = null;
 
 	public Reader getInfo(IMember element, boolean lookIntoParents,
 			boolean lookIntoExternal) {
@@ -39,50 +47,21 @@ public class TclManPagesDocumentationProvider implements
 	}
 
 	public Reader getInfo(String content) {
-		initalizeLocations(false);
-
-		if (folders != null) {
-			for (Iterator iterator = folders.iterator(); iterator.hasNext();) {
-				ManPageFolder f = (ManPageFolder) iterator.next();
-				Map pages = f.getKeywords().map();
-				String ans = (String) pages.get(content);
-				if (ans == null) {
-					// Try to use first word of multiline call
-					if (content.indexOf(' ') != -1) {
-						String subContent = content.substring(0, content
-								.indexOf(' '));
-						ans = (String) pages.get(subContent);
-					}
-				}
-				if (ans != null) {
-					final File file = new File(f.getPath(), ans);
-					if (file.isFile()) {
-						try {
-							return new FileReader(file);
-						} catch (FileNotFoundException e) {
-							e.printStackTrace();
-						}
-					}
-					break;
-				}
-			}
-		}
-
-		return null;
+		return DocumentationUtils.getReader(describeKeyword(content,
+				null));
 	}
 
 	private IPropertyChangeListener changeListener = null;
 
-	private void initalizeLocations(boolean force) {
-		if (!force && this.folders != null)
+	protected void initalizeLocations(boolean force) {
+		if (!force && this.manPages != null)
 			return;
 
 		final IPreferenceStore prefStore = TclUI.getDefault()
 				.getPreferenceStore();
 
-		this.folders = ManPageLoader.load().getDocumentations().get(0)
-				.getFolders();
-		if (this.folders != null && changeListener == null) {
+		this.manPages = ManPageLoader.load();
+		if (this.manPages != null && changeListener == null) {
 			changeListener = new IPropertyChangeListener() {
 				public void propertyChange(PropertyChangeEvent event) {
 					initalizeLocations(true);
@@ -90,5 +69,58 @@ public class TclManPagesDocumentationProvider implements
 			};
 			prefStore.addPropertyChangeListener(changeListener);
 		}
+	}
+
+	private Documentation selectSource(IModelElement context) {
+		initalizeLocations(false);
+		if (manPages == null) {
+			return null;
+		}
+		if (context != null) {
+			final IScriptProject project = context.getScriptProject();
+			if (project != null) {
+				try {
+					final IInterpreterInstall install = ScriptRuntime
+							.getInterpreterInstall(project);
+					final InterpreterDocumentation iDoc = (InterpreterDocumentation) install
+							.findExtension(ManpagesPackage.Literals.INTERPRETER_DOCUMENTATION);
+					if (iDoc != null) {
+						final Documentation doc = manPages.findById(iDoc
+								.getDocumentationId());
+						if (doc != null) {
+							return doc;
+						}
+					}
+				} catch (CoreException e) {
+					TclUI.error(e);
+				}
+			}
+		}
+		return manPages.findDefault();
+	}
+
+	public IDocumentationResponse describeKeyword(String keyword,
+			IModelElement context) {
+		final Documentation documentation = selectSource(context);
+		if (documentation != null) {
+			for (ManPageFolder f : documentation.getFolders()) {
+				String filename = f.getKeywords().get(keyword);
+				if (filename == null) {
+					// Try to use first word only
+					final int spacePos = keyword.indexOf(' ');
+					if (spacePos != -1) {
+						filename = f.getKeywords().get(
+								keyword.substring(0, spacePos));
+					}
+				}
+				if (filename != null) {
+					final File file = new File(f.getPath(), filename);
+					if (file.isFile()) {
+						return new DocumentationFileResponse(keyword, file);
+					}
+				}
+			}
+		}
+		return null;
 	}
 }
