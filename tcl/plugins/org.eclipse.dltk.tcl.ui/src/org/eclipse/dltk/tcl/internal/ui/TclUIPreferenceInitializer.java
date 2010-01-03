c6d0387b50e108260796351eab0e68cab210b087
apanchenk <apanchenk>
2010-01-03 14:30:19 +0000

    init formatter id preference

--- tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/TclUIPreferenceInitializer.java
+++ tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/TclUIPreferenceInitializer.java
@@ -10,25 +10,25 @@
 package org.eclipse.dltk.tcl.internal.ui;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.dltk.compiler.util.Util;
 import org.eclipse.dltk.tcl.ui.TclPreferenceConstants;
 import org.eclipse.dltk.ui.editor.highlighting.SemanticHighlightingUtils;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.ui.editors.text.EditorsUI;
 
-
 public class TclUIPreferenceInitializer extends AbstractPreferenceInitializer {
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
-	 */
 	public void initializeDefaultPreferences() {
 		IPreferenceStore store = TclUI.getDefault().getPreferenceStore();
-		
+
 		EditorsUI.useAnnotationsPreferencePage(store);
 		EditorsUI.useQuickDiffPreferencePage(store);
 		TclPreferenceConstants.initializeDefaultValues(store);
 		SemanticHighlightingUtils.initializeDefaultValues(store, TclUI
 				.getDefault().getTextTools().getSemanticHighlightings());
-	}	
-	
+		store
+				.setDefault(TclPreferenceConstants.FORMATTER_ID,
+						Util.EMPTY_STRING);
+	}
+
 }
