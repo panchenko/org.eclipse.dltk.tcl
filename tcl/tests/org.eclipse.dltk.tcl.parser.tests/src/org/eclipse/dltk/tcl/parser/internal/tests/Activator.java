4368e633cb99f39e63f347e5dd508fb6f946fee0
apanchenk <apanchenk>
2010-01-14 11:49:00 +0000

    @Override

--- tcl/tests/org.eclipse.dltk.tcl.parser.tests/src/org/eclipse/dltk/tcl/parser/internal/tests/Activator.java
+++ tcl/tests/org.eclipse.dltk.tcl.parser.tests/src/org/eclipse/dltk/tcl/parser/internal/tests/Activator.java
@@ -13,26 +13,20 @@ public class Activator extends Plugin {
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
 	public Activator() {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
+	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
+	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
@@ -40,7 +34,7 @@ public class Activator extends Plugin {
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
