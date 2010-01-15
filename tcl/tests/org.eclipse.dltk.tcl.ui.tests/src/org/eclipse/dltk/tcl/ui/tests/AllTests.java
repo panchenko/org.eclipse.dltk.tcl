37b0e74f34bb5c13c6b4bd2303d1c66fd0041644
apanchenk <apanchenk>
2010-01-15 13:30:09 +0000

    rename top level test suites

--- tcl/tests/org.eclipse.dltk.tcl.ui.tests/src/org/eclipse/dltk/tcl/ui/tests/AllTests.java
+++ tcl/tests/org.eclipse.dltk.tcl.ui.tests/src/org/eclipse/dltk/tcl/ui/tests/AllTests.java
@@ -6,14 +6,13 @@ import junit.framework.TestSuite;
 public class AllTests {
 
 	public static Test suite() {
-		TestSuite suite = new TestSuite(
-				"Test for org.eclipse.dltk.tcl.ui.tests");
-		//$JUnit-BEGIN$
-//		suite.addTest(org.eclipse.dltk.tcl.ui.tests.dialogs.AllTests.suite());
+		TestSuite suite = new TestSuite("org.eclipse.dltk.tcl.ui.tests");
+		// $JUnit-BEGIN$
+		// suite.addTest(org.eclipse.dltk.tcl.ui.tests.dialogs.AllTests.suite());
 		suite.addTest(org.eclipse.dltk.tcl.ui.tests.folding.AllTests.suite());
 		suite.addTest(org.eclipse.dltk.tcl.ui.tests.indenting.AllTests.suite());
 		suite.addTest(org.eclipse.dltk.tcl.ui.tests.wizardapi.AllTests.suite());
-		//$JUnit-END$
+		// $JUnit-END$
 		return suite;
 	}
 
