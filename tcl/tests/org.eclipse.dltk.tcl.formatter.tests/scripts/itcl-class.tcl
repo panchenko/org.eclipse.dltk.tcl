afe2b1a8d45f0cc8ced5f7bedd566fc9891ae2fe
apanchenk <apanchenk>
2010-01-15 08:56:42 +0000

    tests added

--- tcl/tests/org.eclipse.dltk.tcl.formatter.tests/scripts/itcl-class.tcl
+++ tcl/tests/org.eclipse.dltk.tcl.formatter.tests/scripts/itcl-class.tcl
@@ -89,11 +89,11 @@ set x "m5"
 }
 ==
 itcl::class A {
-method m1 {} {}
-proc m2 {} {}
-public proc m3 {} {}
-protected proc m4 {} {}
-private proc m5 {} {}
+	method m1 {} {}
+	proc m2 {} {}
+	public proc m3 {} {}
+	protected proc m4 {} {}
+	private proc m5 {} {}
 }
 body A::m1 {} {
 	set x "m1"
@@ -109,4 +109,26 @@ body A::m4 {} {
 }
 body A::m5 {} {
 	set x "m5"
+}
+==== visibility-blocks
+itcl::class A {
+public {
+method m1 {} {
+puts "m1"
+}
+proc m2 {} {
+puts "m2"
+}
+}
+}
+==
+itcl::class A {
+	public {
+		method m1 {} {
+			puts "m1"
+		}
+		proc m2 {} {
+			puts "m2"
+		}
+	}
 }
