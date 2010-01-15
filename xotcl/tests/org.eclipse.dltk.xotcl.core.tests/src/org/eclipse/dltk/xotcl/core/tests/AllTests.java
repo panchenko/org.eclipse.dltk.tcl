a87a5e11df58df9576359d95334bc00afe89dc54
apanchenk <apanchenk>
2010-01-15 13:30:54 +0000

    rename top level test suites

--- xotcl/tests/org.eclipse.dltk.xotcl.core.tests/src/org/eclipse/dltk/xotcl/core/tests/AllTests.java
+++ xotcl/tests/org.eclipse.dltk.xotcl.core.tests/src/org/eclipse/dltk/xotcl/core/tests/AllTests.java
@@ -11,14 +11,13 @@ import junit.framework.TestSuite;
 public class AllTests {
 
 	public static Test suite() {
-		TestSuite suite = new TestSuite(
-				"Test for org.ecipse.dltk.xotcl.core.tests");
-		//$JUnit-BEGIN$
+		TestSuite suite = new TestSuite("org.ecipse.dltk.xotcl.core.tests");
+		// $JUnit-BEGIN$
 		suite.addTestSuite(XOTclCommandProcessorTests.class);
 		suite.addTestSuite(XOTclParserUtilTests.class);
 		suite.addTestSuite(TclCommandProcessorTests.class);
 		suite.addTest(XOTclSelectionTests.suite());
-		//$JUnit-END$
+		// $JUnit-END$
 		return suite;
 	}
 
