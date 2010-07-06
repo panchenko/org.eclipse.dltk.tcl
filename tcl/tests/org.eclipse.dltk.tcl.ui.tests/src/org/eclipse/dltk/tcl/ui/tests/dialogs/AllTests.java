53543027558fbb3712529d944f993cdb26a2b8fb
apanchenk <apanchenk>
2010-07-06 15:48:19 +0000

    don't add NewProjectTestSetup as it's a helper class

--- tcl/tests/org.eclipse.dltk.tcl.ui.tests/src/org/eclipse/dltk/tcl/ui/tests/dialogs/AllTests.java
+++ tcl/tests/org.eclipse.dltk.tcl.ui.tests/src/org/eclipse/dltk/tcl/ui/tests/dialogs/AllTests.java
@@ -8,12 +8,11 @@ public class AllTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite(
 				"Test for org.eclipse.dltk.tcl.ui.tests.dialogs");
-		//$JUnit-BEGIN$
+		// $JUnit-BEGIN$
 		suite.addTestSuite(NewEmptyProjectWizardTest.class);
 		suite.addTest(NewProjectWizardTest.suite());
 		suite.addTestSuite(NewProjectWizardOperationTest.class);
-		suite.addTestSuite(NewProjectTestSetup.class);
-		//$JUnit-END$
+		// $JUnit-END$
 		return suite;
 	}
 
