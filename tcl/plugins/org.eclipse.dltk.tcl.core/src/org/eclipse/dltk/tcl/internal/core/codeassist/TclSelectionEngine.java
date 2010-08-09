c9a2d22dd43abb6c3de52b6eda1fc9c9372125c1
apanchenk <apanchenk>
2010-08-09 17:37:44 +0000

    ISelectionEngine refactoring

--- tcl/plugins/org.eclipse.dltk.tcl.core/src/org/eclipse/dltk/tcl/internal/core/codeassist/TclSelectionEngine.java
+++ tcl/plugins/org.eclipse.dltk.tcl.core/src/org/eclipse/dltk/tcl/internal/core/codeassist/TclSelectionEngine.java
@@ -81,8 +81,8 @@ public class TclSelectionEngine extends ScriptSelectionEngine {
 				.getSelectionExtensions();
 	}
 
-	public IModelElement[] select(IModuleSource sourceUnit,
-			int selectionSourceStart, int selectionSourceEnd) {
+	public void select(IModuleSource sourceUnit, int selectionSourceStart,
+			int selectionSourceEnd) {
 		sourceModule = (org.eclipse.dltk.core.ISourceModule) sourceUnit
 				.getModelElement();
 		String content = sourceUnit.getSourceContents();
@@ -99,7 +99,7 @@ public class TclSelectionEngine extends ScriptSelectionEngine {
 		}
 
 		if (!checkSelection(content, selectionSourceStart, selectionSourceEnd)) {
-			return new IModelElement[0];
+			return;
 		}
 
 		if (DEBUG) {
@@ -154,9 +154,7 @@ public class TclSelectionEngine extends ScriptSelectionEngine {
 				e.printStackTrace(System.out);
 			}
 		}
-
-		return (IModelElement[]) selectionElements
-				.toArray(new IModelElement[selectionElements.size()]);
+		reportModelElements(selectionElements);
 	}
 
 	protected ASTNode parseBlockStatements(TypeDeclaration type,
