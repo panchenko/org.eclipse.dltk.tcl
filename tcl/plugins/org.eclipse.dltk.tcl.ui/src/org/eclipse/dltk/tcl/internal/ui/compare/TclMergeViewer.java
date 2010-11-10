447aac7a1abf0e12a95be2d77d7841fe06d81806
apanchenk <apanchenk>
2010-11-10 23:58:47 +0000

    simplify ScriptMergeViewer for adopters

--- tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/compare/TclMergeViewer.java
+++ tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/compare/TclMergeViewer.java
@@ -12,39 +12,14 @@
 package org.eclipse.dltk.tcl.internal.ui.compare;
 
 import org.eclipse.compare.CompareConfiguration;
-import org.eclipse.dltk.tcl.internal.ui.TclUI;
+import org.eclipse.dltk.tcl.core.TclNature;
 import org.eclipse.dltk.ui.compare.ScriptMergeViewer;
-import org.eclipse.dltk.ui.text.ScriptTextTools;
-import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.widgets.Composite;
 
 public class TclMergeViewer extends ScriptMergeViewer {
 
 	public TclMergeViewer(Composite parent, CompareConfiguration configuration) {
-		super(parent, configuration);
-	}
-
-	protected ScriptTextTools getTextTools() {
-		return TclUI.getDefault().getTextTools();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seeorg.eclipse.dltk.ruby.internal.ui.compare.ScriptMergeViewer#
-	 * getPreferenceStore()
-	 */
-	protected IPreferenceStore getPreferenceStore() {
-		return TclUI.getDefault().getPreferenceStore();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.dltk.ui.compare.ScriptMergeViewer#getTitle()
-	 */
-	public String getTitle() {
-		return Messages.CompareTitle;
+		super(parent, configuration, TclNature.NATURE_ID);
 	}
 
 }
